0cfa3af803bde7fcfb353906dd309c094f4ce9cd
Ninmar Olpenado <nyanmarrolpenado@gmail.com>
2026-08-17 08:55:33 +0800

    - completed audit logs migration

--- database/migrations/2026_01_01_000004_audit_logs.php
+++ database/migrations/2026_01_01_000004_audit_logs.php
@@ -8,11 +8,25 @@ return new class extends Migration
 {
     public function up(): void
     {
-        //
+        Schema::create('audit_logs', function (Blueprint $table) {
+            $table->string('audit_log_id')->primary();
+            $table->foreignId('user_id')->constrained('users')->references('user_id');
+            $table->enum(
+                'audit_action_type',
+                [
+                    'INSERT',
+                    'UPDATE',
+                    'DELETE'
+                ]
+            )->default('INSERT');
+            $table->text('target');
+            $table->json('metadata')->nullable();
+            $table->timestamps();
+        });
     }
 
     public function down(): void
     {
-        //
+        Schema::dropIfExists('audit_logs');
     }
 };
