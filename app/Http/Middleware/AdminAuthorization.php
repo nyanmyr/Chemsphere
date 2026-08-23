158f568c63159d6ef14db69e18de8c7b0e24612d
Ninmar Olpenado <nyanmarrolpenado@gmail.com>
2026-08-23 12:06:32 +0800

    - fixed AdminAuthorization to use isAdmin() enum method

--- app/Http/Middleware/AdminAuthorization.php
+++ app/Http/Middleware/AdminAuthorization.php
@@ -10,7 +10,7 @@ class AdminAuthorization
 {
     public function handle(Request $request, Closure $next): Response
     {
-        if (! $request->user() || $request->user()->user_role !== 'admin') {
+        if (!$request->user() || !$request->user()->user_role?->isAdmin()) {
             return redirect()->back(fallback: route('welcome'));
         }
 
