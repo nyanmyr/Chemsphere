85abdd906c61a76f9d0027ed723bb8e24f73360a
Ninmar Olpenado <nyanmarrolpenado@gmail.com>
2026-08-29 10:33:45 +0800

    - added error messaging for create chemical form

--- resources/views/create_chemical.blade.php
+++ resources/views/create_chemical.blade.php
@@ -14,50 +14,90 @@
         <br>
         <input type="number" id="location_id" name="location_id" required>
 
+        @error('location_id')
+        <div>{{ $message }}</div>
+        @enderror
+
         <br>
         <label for="created_by">Created By</label>
         <br>
         <input type="number" id="created_by" name="created_by" required>
+
+        @error('created_by')
+        <div>{{ $message }}</div>
+        @enderror
 
         <br>
         <label for="chemical_name">Name</label>
         <br>
         <input type="text" id="chemical_name" name="chemical_name" required>
 
+        @error('chemical_name')
+        <div>{{ $message }}</div>
+        @enderror
+
         <br>
         <label for="batch_number">Batch Number</label>
         <br>
         <input type="text" id="batch_number" name="batch_number" required>
+
+        @error('batch_number')
+        <div>{{ $message }}</div>
+        @enderror
 
         <br>
         <label for="brand_name">Brand Name</label>
         <br>
         <input type="text" id="brand_name" name="brand_name" required>
 
+        @error('brand_name')
+        <div>{{ $message }}</div>
+        @enderror
+
         <br>
         <label for="volume_per_unit">Volume Per Unit</label>
         <br>
         <input type="number" id="volume_per_unit" name="volume_per_unit" step="0.001" min="0" max="9999999999" required>
+
+        @error('volume_per_unit')
+        <div>{{ $message }}</div>
+        @enderror
 
         <br>
         <label for="initial_quantity">Initial Quantity</label>
         <br>
         <input type="number" id="initial_quantity" name="initial_quantity" step="0.001" min="0" max="9999999999" required>
 
+        @error('initial_quantity')
+        <div>{{ $message }}</div>
+        @enderror
+
         <br>
         <label for="current_quantity">Current Quantity</label>
         <br>
         <input type="number" id="current_quantity" name="current_quantity" step="0.001" min="0" max="9999999999" required>
+
+        @error('current_quantity')
+        <div>{{ $message }}</div>
+        @enderror
 
         <br>
         <label for="expiration_date">Expiration Date</label>
         <br>
         <input type="date" id="expiration_date" name="expiration_date" required>
 
+        @error('expiration_date')
+        <div>{{ $message }}</div>
+        @enderror
+
         <br>
         <label for="arrival_date">Arrival Date</label>
         <br>
         <input type="date" id="arrival_date" name="arrival_date" required>
+
+        @error('arrival_date')
+        <div>{{ $message }}</div>
+        @enderror
 
         <br>
         <label>Safety Classes</label>
