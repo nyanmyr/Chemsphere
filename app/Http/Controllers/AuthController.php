b8eaed3f2ac2daa1750c5120e6c364d0a8c6cded
Ninmar Olpenado <nyanmarrolpenado@gmail.com>
2026-09-07 08:12:22 +0800

    - updated register function in AuthController to only allow emails of @uic.edu.ph

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,7 +15,7 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $credentials = $request->validate([
-            'email' => 'required|email|unique:users,email',
+            'email' => 'required|email|unique:users,email|ends_with:@uic.edu.ph',
             'password' => 'required|min:6',
         ]);
 
